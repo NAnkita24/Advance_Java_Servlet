aba6339ef5bf7f1d2517f6dc7a964fa9ace13282
NAnkita24 <apjagtap24@gmail.com>
2024-01-31 22:48:48 +0530

    31-January

--- src/main/java/Package1/Servlet2.java
+++ src/main/java/Package1/Servlet2.java
@@ -2,9 +2,11 @@ package Package1;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.jar.Attributes.Name;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,8 +15,31 @@ import jakarta.servlet.http.HttpServletResponse;
 public class Servlet2  extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter pw=resp.getWriter();
-		String name=req.getParameter("name");
+		
+		Cookie [] cookies=req.getCookies();
+		boolean f=false;
+		String name="";
+		
+		if (cookies==null) {
+			pw.println("<h1>you are a new user so go to home page</h1>");
+			return;
+		}
+		else {
+			for(Cookie c  :cookies ) {
+				
+				String str=c.getName();
+				if (str.equals("username")) {
+					f=true;
+					name=c.getValue();
+					
+				}
+			}
+		}
+		
+		if (f) {
+		
 		pw.println("<h1> Hii ,"+name+"Welcome  back to website </h1>");
 		pw.println("<h1> Thank you</h1>");
+		}
 }
 }
